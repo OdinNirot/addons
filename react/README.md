8f7a3a74eed7b73dfdd64d6440c155bea309148a
OdinNirot <>
2023-11-27 13:53:29 -0500

    added react files

---
+++ react/README.md
@@ -0,0 +1,1 @@
+The React addon can be found at: https://github.com/SammehFFXI/FFXIAddons/tree/master/React
---
+++ react/react_RDM.lua
@@ -0,0 +1,50 @@
+return {
+    ["Aita"]={
+        ["Eroding Flesh"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee wind"
+        }, 
+        ["Flashflood"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee thunder"
+        }, 
+        ["Icy Grasp"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee fire"
+        }, 
+        ["Flaming Kick"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee water"
+        }, 
+        ["Fulminous Smash"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee earth"
+        }
+    }
+    ["Aminon"]={
+        ["Ensepulcher"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee wind"
+        }, 
+        ["Torrential Pain"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee thunder"
+        }, 
+        ["Frozen Blood"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee fire"
+        }, 
+        ["Demonfire"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee water"
+        }, 
+        ["Ceaseless Surge"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee earth"
+        }
+        ["Blast of Reticence"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee ice"
+        }
+    }
+}
