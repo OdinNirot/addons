44fd7b9b3a5069608b5a0be7430104999b76865c
OdinNirot <>
2023-11-27 19:35:31 -0500

    fixed a comma causing errors

--- react/react_RDM.lua
+++ react/react_RDM.lua
@@ -1,26 +1,4 @@
 return {
-    ["Aita"]={
-        ["Eroding Flesh"]={
-            ["complete_reaction"]="", 
-            ["ready_reaction"]="ee wind"
-        }, 
-        ["Flashflood"]={
-            ["complete_reaction"]="", 
-            ["ready_reaction"]="ee thunder"
-        }, 
-        ["Icy Grasp"]={
-            ["complete_reaction"]="", 
-            ["ready_reaction"]="ee fire"
-        }, 
-        ["Flaming Kick"]={
-            ["complete_reaction"]="", 
-            ["ready_reaction"]="ee water"
-        }, 
-        ["Fulminous Smash"]={
-            ["complete_reaction"]="", 
-            ["ready_reaction"]="ee earth"
-        }
-    }
     ["Aminon"]={
         ["Ensepulcher"]={
             ["complete_reaction"]="", 
@@ -41,10 +19,32 @@ return {
         ["Ceaseless Surge"]={
             ["complete_reaction"]="", 
             ["ready_reaction"]="ee earth"
-        }
+        },
         ["Blast of Reticence"]={
             ["complete_reaction"]="", 
             ["ready_reaction"]="ee ice"
+        }
+	}, 
+    ["Aita"]={
+        ["Eroding Flesh"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee wind"
+        }, 
+        ["Flashflood"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee thunder"
+        }, 
+        ["Icy Grasp"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee fire"
+        }, 
+        ["Flaming Kick"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee water"
+        }, 
+        ["Fulminous Smash"]={
+            ["complete_reaction"]="", 
+            ["ready_reaction"]="ee earth"
         }
     }
 }
